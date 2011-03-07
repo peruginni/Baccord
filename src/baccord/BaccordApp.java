fb712f84d8d753bc6303cab52486e8555f4086c2
Ondřej Macoszek <ondra.macoszek@gmail.com>
2011-03-07 13:52:40 +0100

    initial commit with prepared empty project

---
+++ src/baccord/BaccordApp.java
@@ -0,0 +1,44 @@
+/*
+ * BaccordApp.java
+ */
+
+package baccord;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class BaccordApp extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new BaccordView(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of BaccordApp
+     */
+    public static BaccordApp getApplication() {
+        return Application.getInstance(BaccordApp.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(BaccordApp.class, args);
+    }
+}
